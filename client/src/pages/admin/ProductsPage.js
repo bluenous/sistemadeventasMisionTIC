1fc07e7d839c6822bdf54e9c97070598ccb5bd10
Danny Gómez <styven.gomez@udea.edu.co>
2021-09-28 19:19:31 -0500

    Entré en mi página para editar

--- client/src/pages/admin/ProductsPage.js
+++ client/src/pages/admin/ProductsPage.js
@@ -1,8 +1,7 @@
 export default function ProductsPage() {
-    return(
-        <div>
-            <h1>ProductsPage</h1>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>ProductsPage Danny</h1>
+    </div>
+  );
 }
